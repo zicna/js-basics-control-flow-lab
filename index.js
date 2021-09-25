623e50a1dc846ffe486c48d8a4a64ab67bf29afa
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-25 10:42:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(deep) {
   // Write your code here!
+  // if(deep < 400){
+  //   return "This one is on me!"
+  // }else if(deep < 2500 ){
+  //   return 'I will gladly take your thirty bucks.'
+  // }else{
+  //   return "No can do."
+  // }
+  if (deep < 400) return "This one is on me!";
+  else if (deep < 2500) return "I will gladly take your thirty bucks.";
+  else return "No can do.";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+if(city === "NYC") return "Ok, sounds good."
+else return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:  return "Bye."
+  }
 }
