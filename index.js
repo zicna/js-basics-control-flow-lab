6dd005dba5c5ba7ac907ce30a544e4f593929b15
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-25 10:41:53 -0500

    commit tree e730e392a9d901aeed2ce54dcbc552af518f0c30

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(deep) {
   // Write your code here!
+  // if(deep < 400){
+  //   return "This one is on me!"
+  // }else if(deep < 2500 ){
+  //   return 'I will gladly take your thirty bucks.'
+  // }else{
+  //   return "No can do."
+  // }
+  if (deep < 400) return "This one is on me!";
+  else if (deep < 2500) return "I will gladly take your thirty bucks.";
+  else return "No can do.";
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+if(city === "NYC") return "Ok, sounds good."
+else return "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  // if (tip > 20) return "Thank you so much."
+  // else if(15 < tip < 20) return "Thank you."
+  // else return "Bye."
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:  return "Bye."
+  }
 }
